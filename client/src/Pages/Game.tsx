68565a78d3a90f3ee53e4720a8d21f2d2fa391db
Kamil Marczak <kamil.codemate@gmail.com>
2023-02-21 10:30:36 +0100

    Add card rendering

--- client/src/Pages/Game.tsx
+++ client/src/Pages/Game.tsx
@@ -1,5 +1,6 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from '../components/Card';
 import '../assets/styles/Game.scss';
 type card = {
   column: number;
@@ -11,16 +12,23 @@ const Game: React.FC<{}> = (): React.ReactElement => {
   const [gameData, setGameData] = useState<{ gameId: string; playerId: string; playerNo: number | string }>(
     JSON.parse(localStorage.getItem('gameData') as string)
   );
-  const [cardsPos, setcardPos] = useState<Array<card>>(null as any);
+  const [cardsPos, setcardPos] = useState<Array<card>>([]);
   useEffect(() => {
     console.log(gameData);
 
     const updateGame = async () => {
       try {
         const response = await axios.post('/api/game', { gameId: gameData.gameId, playerId: gameData.playerId, playerNo: gameData.playerNo });
-        console.log(response);
+
         const cards = response.data.cards;
-        setcardPos(cards);
+        let cardSorted: Array<card> = [];
+        for (let i = 0; i < 8; i++) {
+          for (let j = 0; j < 6; j++) {
+            cardSorted.push(cards[cards.findIndex((element: card) => element.column === i && element.row === j)]);
+          }
+        }
+        console.log(cardSorted);
+        setcardPos(cardSorted);
       } catch (err) {
         console.log(err);
       }
@@ -31,7 +39,15 @@ const Game: React.FC<{}> = (): React.ReactElement => {
       clearInterval(sendReq);
     };
   }, []);
-  return <div></div>;
+  return (
+    <div className='gameContainer'>
+      <div className='cardContainer'>
+        {cardsPos.map((element) => {
+          return <Card isShowed={element.isShowed} identifier={element.correctIndentifier} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Game;
--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Images, { ImgType } from '../assets/images/imgExport';
-
+import '../assets/styles/Card.scss';
 type Props = {
   identifier: number;
   isShowed: boolean;
@@ -10,9 +10,15 @@ type ImageKey = keyof ImgType;
 
 const Card: React.FC<Props> = ({ identifier, isShowed }): React.ReactElement => {
   let displayImage: string;
-  if (isShowed) displayImage = Images[`img${identifier}` as ImageKey];
-  if (isShowed) return <div className='card'></div>;
-  return <></>;
+  let returnContent: React.ReactElement;
+  if (isShowed) {
+    displayImage = Images[`img${identifier}` as ImageKey];
+    returnContent = <div className='card' style={{ backgroundImage: `url(${displayImage})` }}></div>;
+  } else {
+    returnContent = <div className='card'></div>;
+  }
+
+  return returnContent;
 };
 
 export default Card;
