3f7cdd5ce2a2bd4a9014bb298eb7b09cf38bf3b8
Kamil Marczak <kamil.codemate@gmail.com>
2023-02-23 14:28:21 +0100

    Preloading card images

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,6 +1,7 @@
-import React, { MouseEventHandler, useEffect } from 'react';
+import React, { MouseEventHandler, Suspense } from 'react';
 import Images, { ImgType } from '../assets/images/imgExport';
 import '../assets/styles/Card.scss';
+
 type Props = {
   identifier: number;
   isShowed: boolean;
@@ -10,20 +11,21 @@ type Props = {
 type ImageKey = keyof ImgType;
 
 const Card: React.FC<Props> = ({ identifier, isShowed, handleClick }): React.ReactElement => {
-  useEffect(() => {
-    console.log(Images);
-  }, []);
+  let displayImage: string | null = null;
 
-  let displayImage: string;
-  let returnContent: React.ReactElement;
   if (isShowed) {
     displayImage = Images[`img${identifier + 1}` as ImageKey];
-    returnContent = <div className='card' style={{ backgroundImage: `url(${displayImage})` }} onClick={handleClick}></div>;
-  } else {
-    returnContent = <div className='card' onClick={handleClick}></div>;
   }
 
-  return returnContent;
+  return (
+    <div className='card' style={{ backgroundImage: `url(${displayImage})` }} onClick={handleClick}>
+      {!displayImage && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <img src={Images[`img${identifier + 1}` as ImageKey]} alt={`Card ${identifier}`} style={{ display: 'none' }} />
+        </Suspense>
+      )}
+    </div>
+  );
 };
 
 export default Card;
